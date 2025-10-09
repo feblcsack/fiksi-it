e701bcb4513e8b55185f3aae345513cc3f77e99f
feblcsack <alifianabdiel@gmail.com>
2025-10-10 00:36:58 +0700

    upd

--- src/app/(main)/dashboard/user/page.tsx
+++ src/app/(main)/dashboard/user/page.tsx
@@ -4,10 +4,11 @@ import { NearbyGigsList } from "@/components/features/NearbyGigsList";
 export default function UserDashboardPage() {
   return (
     <>
-      <Navbar />
+    <div className="fixed top-0 w-full z-20">   <Navbar /></div>
+   
 
 <div>
-      <div className="max-w-6xl mx-auto px-6 py-16">
+      <div className="max-w-6xl mx-auto px-6 py-16 pt-16 md:pt-16">
         <div className="mb-16">
           <h2 className="text-5xl font-light tracking-tight mb-4 font-lora">
             Live Near You
