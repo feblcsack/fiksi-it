7787db91cb6da7dd184215ec82c533674076b819
feblcsack <alifianabdiel@gmail.com>
2025-10-09 11:23:22 +0700

    error

--- src/app/404/page.tsx
+++ src/app/404/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Suspense } from "react";
-import NotFoundContent from "./NotFoundContent";
+import NotFoundContent from "../NotFoundContent";
 
 export default function NotFoundPage() {
   return (
